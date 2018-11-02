0e6ff1de35f854360a57ac382fe72b1f9b9a5826
Chukwuemeka Nwagu <c_nwagu@yahoo.com>
2018-11-02 03:03:18 +0100

    change supported abis

--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -1,4 +1,4 @@
 #APP_PROJECT_PATH := $(call my-dir)/project
 #APP_MODULES      := chess-jni
-APP_ABI          := armeabi armeabi-v7a x86 mips
+APP_ABI          := armeabi-v7a, arm64-v8a, x86, x86_64
 APP_STL         := c++_static
