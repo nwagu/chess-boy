556eb7ee56fd06663597e009387110289d5f8301
Chukwuemeka Nwagu <developer.nwagu@gmail.com>
2021-10-07 23:46:44 +0100

    dependency updates, etc

--- buildSrc/src/main/kotlin/com/nwagu/chessboy/buildsrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/nwagu/chessboy/buildsrc/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val kotlin_version = "1.5.21"
-    const val gradle_plugin_version = "7.1.0-alpha06"
+    const val gradle_plugin_version = "7.1.0-alpha13"
     const val sql_delight_version = "1.5.0"
     const val compose_version = "1.0.1"
     const val google_services_version = "4.3.8"
